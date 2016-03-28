3d03702f28065e3500413e64f7113d6b0a96050e
initas <febri.init@gmail.com>
2016-03-28 10:05:46 +0700

    Dummy Data

--- data/atur-dana.sql
+++ data/atur-dana.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 26, 2016 at 07:51 PM
+-- Generation Time: Mar 28, 2016 at 05:04 AM
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.6.14
 
@@ -31,7 +31,7 @@ CREATE TABLE `sources` (
   `user_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `icon_class` varchar(20) NOT NULL,
-  `hex_color` varchar(6) NOT NULL,
+  `hex_color` varchar(7) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status_id` int(11) NOT NULL DEFAULT '1'
@@ -42,8 +42,9 @@ CREATE TABLE `sources` (
 --
 
 INSERT INTO `sources` (`id`, `user_id`, `name`, `icon_class`, `hex_color`, `created_at`, `updated_at`, `status_id`) VALUES
-(1, 1, 'ATM BCA', 'fa fa-heart', 'ffff00', '2016-03-15 10:32:36', '2016-03-15 10:34:13', 1),
-(2, 1, 'Dompet', 'fa fa-heart', 'ffff00', '2016-03-15 10:32:36', '2016-03-15 10:34:16', 1);
+(1, 1, 'ATM BCA', 'fa fa-list-alt', '#E74C3C', '2016-03-15 10:32:36', '2016-03-28 02:32:06', 1),
+(2, 1, 'Dompet', 'fa fa-archive', '#3497D9', '2016-03-15 10:32:36', '2016-03-28 02:33:10', 1),
+(3, 1, 'ATM Mandiri', 'fa fa-bank', '#f39c12', '2016-03-28 02:49:20', '2016-03-28 02:49:56', 1);
 
 -- --------------------------------------------------------
 
@@ -73,9 +74,9 @@ CREATE TABLE `transactions` (
   `description` text,
   `image_url` text,
   `geo_location` varchar(255) DEFAULT NULL,
-  `latitude` float DEFAULT NULL,
-  `longitude` float DEFAULT NULL,
-  `altitude` float DEFAULT NULL,
+  `latitude` varchar(16) DEFAULT NULL,
+  `longitude` varchar(16) DEFAULT NULL,
+  `altitude` varchar(16) DEFAULT NULL,
   `transaction_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -87,8 +88,9 @@ CREATE TABLE `transactions` (
 --
 
 INSERT INTO `transactions` (`id`, `user_id`, `source_id`, `transaction_category_id`, `amount`, `description`, `image_url`, `geo_location`, `latitude`, `longitude`, `altitude`, `transaction_at`, `created_at`, `updated_at`, `status_id`) VALUES
-(1, 1, 1, 1, 1, 'updated dummy description', NULL, NULL, NULL, NULL, NULL, '2016-03-13 17:55:17', '2016-03-11 20:10:48', '2016-03-15 10:53:55', 0),
-(56, 2, 1, 1, 1, 'dummy description', NULL, '', 0, 0, 0, '0000-00-00 00:00:00', '2016-03-26 08:38:24', '2016-03-26 08:38:24', 1);
+(1, 1, 1, 2, 50000, NULL, NULL, NULL, NULL, NULL, NULL, '2016-03-13 17:55:17', '2016-03-11 20:10:48', '2016-03-28 02:47:55', 1),
+(2, 2, 2, 1, 250000, 'Lumayan baru gajian', NULL, 'Sate Khas Senayan', '-6.2173012', '106.7838386', NULL, '2016-03-15 02:27:18', '2016-03-26 08:38:24', '2016-03-28 03:03:57', 1),
+(3, 1, 1, 1, 700000, 'Tarik tunai', NULL, NULL, NULL, NULL, NULL, '2016-03-28 02:34:31', '2016-03-28 02:34:31', '2016-03-28 03:04:00', 1);
 
 -- --------------------------------------------------------
 
@@ -101,7 +103,7 @@ CREATE TABLE `transaction_categories` (
   `user_id` int(11) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   `icon_class` varchar(20) NOT NULL,
-  `hex_color` varchar(6) NOT NULL,
+  `hex_color` varchar(7) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status_id` int(11) NOT NULL DEFAULT '1'
@@ -112,9 +114,9 @@ CREATE TABLE `transaction_categories` (
 --
 
 INSERT INTO `transaction_categories` (`id`, `user_id`, `name`, `icon_class`, `hex_color`, `created_at`, `updated_at`, `status_id`) VALUES
-(1, NULL, 'makan', 'fa fa-cutlery', 'ffff00', '2016-03-12 17:34:01', '2016-03-12 21:04:17', 1),
-(2, 1, 'dummy', 'fa fa-heart', 'ff00ff', '2016-03-12 14:07:30', '2016-03-12 14:07:30', 1),
-(3, 1, 'dummy', 'fa fa-heart', 'ff00ff', '2016-03-12 14:07:38', '2016-03-12 14:07:38', 1);
+(1, NULL, 'Makan', 'fa fa-cutlery', '#F5A623', '2016-03-12 17:34:01', '2016-03-28 02:50:24', 1),
+(2, NULL, 'Telepon', 'fa fa-phone', '#4A90E2', '2016-03-12 14:07:30', '2016-03-28 02:50:28', 1),
+(3, NULL, 'Transport', 'fa fa-bus', '#E74C3C', '2016-03-12 14:07:38', '2016-03-28 02:50:31', 1);
 
 -- --------------------------------------------------------
 
@@ -126,7 +128,7 @@ CREATE TABLE `transaction_comments` (
   `id` int(11) NOT NULL,
   `transaction_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `description` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `status_id` int(11) NOT NULL DEFAULT '1'
@@ -137,7 +139,7 @@ CREATE TABLE `transaction_comments` (
 --
 
 INSERT INTO `transaction_comments` (`id`, `transaction_id`, `user_id`, `description`, `created_at`, `updated_at`, `status_id`) VALUES
-(1, 1, 1, 1, '2016-03-12 17:40:39', '2016-03-12 17:40:39', 1);
+(1, 2, 2, 'Wih banyak duit nih...', '2016-03-24 17:40:39', '2016-03-28 02:52:42', 1);
 
 -- --------------------------------------------------------
 
@@ -162,7 +164,7 @@ CREATE TABLE `users` (
   `last_login_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `status_id` int(11) NOT NULL DEFAULT '0'
+  `status_id` int(11) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -170,8 +172,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fb_id`, `unique_id`, `email`, `username`, `password`, `full_name`, `birthday`, `gender_id`, `image_url`, `cover_image_url`, `auth_token`, `point_total`, `last_login_at`, `created_at`, `updated_at`, `status_id`) VALUES
-(1, NULL, '1', 'admin@situs.id', 'admin', '2133fc4d88502222bbf42c116a20700b4d78e0f1cbd4b09e6f23460b4525fa7c2f', 'Inara Risyah', NULL, NULL, NULL, NULL, 'c7c3565bef6a5cf4c562d672687cd8e8', 0, '2016-02-11 00:27:45', '2016-02-04 09:43:25', '2016-03-12 19:03:20', 0),
-(2, NULL, '2', 'febri@situs.id', 'mf', '2133fc4d88502222bbf42c116a20700b4d78e0f1cbd4b09e6f23460b4525fa7c2f', 'muhammad febriansyah', NULL, NULL, NULL, NULL, '7abcf6fe946c0ecbb0df9ed7ccc2a465', 0, NULL, '2016-02-04 09:43:25', '2016-03-12 19:03:14', 0);
+(1, NULL, '1', 'admin@situs.id', 'admin', '2133fc4d88502222bbf42c116a20700b4d78e0f1cbd4b09e6f23460b4525fa7c2f', 'Inara Risyah', NULL, NULL, NULL, NULL, 'c7c3565bef6a5cf4c562d672687cd8e8', 0, '2016-02-11 00:27:45', '2016-02-04 09:43:25', '2016-03-28 02:53:36', 1),
+(2, NULL, '2', 'yuna@situs.id', 'mf', '2133fc4d88502222bbf42c116a20700b4d78e0f1cbd4b09e6f23460b4525fa7c2f', 'Yuna Fiyina', NULL, NULL, NULL, NULL, '7abcf6fe946c0ecbb0df9ed7ccc2a465', 0, NULL, '2016-02-04 09:43:25', '2016-03-28 03:04:24', 1);
 
 --
 -- Indexes for dumped tables
@@ -222,12 +224,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `sources`
 --
 ALTER TABLE `sources`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `transaction_categories`
 --
